cfd373d4b9f0ca4f695cd296e39f1e3eb5154f77
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-02-24 17:12:58 -0500

    Create form element

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+export default function Form(props) {
+
+  // Tracking the Form State and adding Default State
+  // useState is a custom hook store it as a memory so when the function is run, it doesn't go away. it remembers it and re use it when you refresh a browser page.
+  const [name, setName] = useState(props.name || "")
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+  
+  //Add a reset() function to the Form component that sets student and interviewer to "".
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  }
+  
+  //Add a cancel function to the Form component that calls reset() and props.onCancel. We should also update our Form component so it's called when a user clicks the Cancel button.
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+
+  // empty student name and unselected interviewer should not save
+  function saveError() {
+    if (name === "") {
+      setError("Name can't be blank.")
+      return;
+    }
+    if (interviewer === null) {
+      setError("Interviewer can't be unselected.")
+      return;
+    }
+    props.onSave(name, interviewer)
+    setError("");
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault}>
+          <input
+            value={name}
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </form>
+
+        <section className="appointment__validation">
+          {error}
+        </section>
+
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={() => cancel(name)}>Cancel</Button>
+          <Button confirm onClick={saveError}>Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+}
