46e84a8a0abd0bed38e3c13860b35c5c16c96fb5
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-02-27 14:36:08 -0500

    custum hook npm install

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,35 @@
+// import { useState } from "react";
+
+
+// export default function useVisualMode(initial) {
+//   const [mode, setMode] = useState(initial);
+  
+
+//   return { mode };
+// }
+
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      setMode(newMode);
+      setHistory((prev) => [...prev]);
+    } else {
+      setMode(newMode);
+      setHistory((prev) => [...prev, newMode]);
+    }
+  };
+
+  const back = () => {
+    if (history.length !== 0) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+  };
+
+  return { mode, transition, back };
+}
