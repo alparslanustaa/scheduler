6243a2566896223707298547c4214fb6bf28cbae
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-03-02 16:43:48 -0500

    test file for Appointment if it renders or fails

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -4,8 +4,20 @@ import Appointment from "components/Appointment";
 
 afterEach(cleanup);
 
-describe("Appointment", ()=> {
+describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
   });
+  
+  it("doesn't call the function", () => {
+    const fn = jest.fn();
+    expect(fn).toHaveBeenCalledTimes(0);
+  });
+
+  it("uses the mock implementation", () => {
+    const fn = jest.fn((a, b) => 42);
+    fn(1, 2);
+    expect(fn).toHaveReturnedWith(42);
+   });
+   
 });
