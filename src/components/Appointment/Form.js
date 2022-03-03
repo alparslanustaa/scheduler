e4b508cc9513d17823133f4618139a1623d636bb
Alparslan Bahadir Usta <93120939+alparslanustaa@users.noreply.github.com>
2022-03-03 18:08:54 -0500

    form element bug fix for interviewer

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -29,6 +29,12 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+
+    if (interviewer === null) {
+      setError("Interviewer name cannot be blank");
+      return;
+    }
+    
     setError("");
     props.onSave(name, interviewer);
   }
